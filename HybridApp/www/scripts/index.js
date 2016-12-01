78c61600f1a5aa7c49fc958cb1f84a2c0185f053
Nilay <mehta@DESKTOP-Q07ETC1>
2016-12-01 12:53:13 +0530

    Hello World

--- HybridApp/www/scripts/index.js
+++ HybridApp/www/scripts/index.js
@@ -13,11 +13,7 @@
         document.addEventListener( 'resume', onResume.bind( this ), false );
         
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        
     };
 
     function onPause() {
