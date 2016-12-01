b78fcf6f74ba060acead629f548cf2986860bc01
Nilay <mehta@DESKTOP-Q07ETC1>
2016-12-01 19:03:08 +0530

    Add MobiScroll Suport

--- HybridApp/www/scripts/index.js
+++ HybridApp/www/scripts/index.js
@@ -2,25 +2,15 @@
 // http://go.microsoft.com/fwlink/?LinkID=397704
 // To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
 // and then run "window.location.reload()" in the JavaScript Console.
-(function () {
+(function (window) {
     "use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
-
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        
-    };
-
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
-
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-} )();
+    $(".title").text("MNilay");
+    $("#calendar").val("Helloooo");
+    try {
+        $('#calendar').mobiscroll().calendar();
+    }
+    catch (e) {
+        $("body").innerHTML = "Missing MobiScroll";
+    }
+} )(window);
