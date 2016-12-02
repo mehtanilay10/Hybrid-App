6f0986579d208698b3b9b79a3b1815cd94f6cef9
Nilay <mehta@DESKTOP-Q07ETC1>
2016-12-02 15:50:56 +0530

    load product listing dynamically

--- HybridApp/www/scripts/index.js
+++ HybridApp/www/scripts/index.js
@@ -5,24 +5,25 @@
 (function (window) {
     "use strict";
 
-    $(".title").text("MNilay");
-    $("#calendar").val("Helloooo");
-
+    //$(".title").text("MNilay");
+    
+    
     try {
         // Deside Theme for App
         var theme = 'mobiscroll';
+        // mobiscroll | mobiscroll-dark | material | material-dark | ios | ios-dark | wp | wp-light | android-holo-light
 
         // Set Theme for App
         mobiscroll.settings = {
             theme: theme
         };
-
+        /*
         // Set Calender Dropdown
         $('#calendar').mobiscroll().calendar({
             theme: theme,
             display: 'top'
         });
-
+        */
         // Product List for Homepage
         $(function () {
             $('.product-list').mobiscroll().menustrip({
@@ -33,6 +34,30 @@
                 itemWidth: 150
             });
         });
+
+        // Populate from JSON
+        $.getJSON("scripts/resources.resjson")
+            .done(function (json) {
+                console.log("JSON Data: " + JSON.stringify(json, null, 4));
+                $.each(json.products, function (i, item) {
+                    // Create Element
+                    var li = document.createElement("li");
+                    var a = $('<a>', {href: 'view-details.html?id=?' + item.id}).appendTo(li);
+                    $('<img>', { class: 'list-img', src: 'images/img_' + item.image, height: '115' }).appendTo(a);
+                    $('<div>', {class: 'list-title', text: item.title}).appendTo(a)
+                    $('<div>', { class: 'list-price', text: item.price }).appendTo(a);
+
+                    // Based on type select wher to place element
+                    if (item.category == "Mobile")
+                        $(li).appendTo("#mobile-list");
+                    else
+                        $(li).appendTo("#laptop-list");
+                });
+            })
+            .fail(function (jqxhr, textStatus, error) {
+                var err = textStatus + ", " + error;
+                console.log("Request Failed: " + err);
+            });
     }
     catch (e) {
         $("body").innerHTML = "Missing MobiScroll";
